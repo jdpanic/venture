3bf5122b7bf05eebf4621650bd7b75edacf640a0
James Dietz <james.j.dietz@gmail.com>
2012-03-21 19:54:21 -0400

    Added a urls.py for the app, and more instructions in the README.

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import patterns, include, url
+
+urlpatterns = pattern(
+    url(r'^game$', 'venture.views.game'),
+    url(r'^action/$', 'venture.views.action'),
+    url(r'^quit/$', 'venture.views.quit'),
+    url(r'^login/$', 'venture.views.login'),
+    url(r'^$', venture.views.main),
+)
