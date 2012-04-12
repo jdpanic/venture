4ee230e34fdc62bad5949af2df25fbd459713064
James Dietz <james.j.dietz@gmail.com>
2012-04-11 20:18:49 -0400

    URLs now work propperly.

--- models.py
+++ models.py
@@ -1,22 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 class Person(models.Model):
  """ This model represents all characters (players and NPCs) in the game. """
  name = models.CharField(max_length=50, unique=True)
+ user = models.ForeignKey(User, null=True) # If it's null this person is an NPC
  alive = models.BooleanField(default=True) # alive on creation
  room = models.ForeignKey('Room') # where am i?
- #items = models.ForeignKey('Item', related_name='owner')
- # score = models.IntegerField(blank=True)
+ money = models.IntegerField(default=0) # in-game currency
+ items = models.ManyToManyField('Item')
+ quests = models.ManyToManyField('Quest') # Each person can be on many quests at once
  def __unicode__(self):
   return self.name
 
  def take(self, what): # take an item
-  #what.room = None # it belongs to this person now
-  if what.room == self.room and what.takeable:
-   what.owner = self
-   return True
+  if what.room == self.room and self.items.objects.get(pk=what.id) is None:
+   self.items.add(what)
+   if not what.static:
+    what.visible = False # No one else can see or take this item
+   # check to see if this item is in any quests.
+   for q in self.quests:
+    try:
+     obj = q.items.objects.get(pk=what.id)
+     
+    except:
+     pass
+    
+   return "You are now carrying " + what.name
   else:
-   return False
+   return None
 
 class Room(models.Model):
  description = models.TextField()  # You are in the drawing room. Burning fires are everywhere.
@@ -29,7 +42,8 @@ class Exit(models.Model):
  from_room = models.ForeignKey(Room, related_name="exit")
  to_room = models.ForeignKey(Room, related_name="entrance")
  description = models.TextField() # A spiral staircase leads to _
- opened = models.BooleanField(default=True, help_text="Is this exit opened or closed?")
+ locked = models.BooleanField(default=False)
+ key_item = models.ForeignKey('Item', help_text="If locked is True, the player needs this item to get through the exit.")
  transition_message = models.TextField( blank=True, help_text="You step through the door.")
  def __unicode__(self):
   return "from " + self.from_room + " to " + self.to_room
@@ -39,12 +53,17 @@ class Item(models.Model):
  description = models.TextField(blank=True, null=True, help_text="Description as seen in person's inventory.")
  inroom_description = models.TextField(blank=True, null=True, help_text="Optional item description in a room e.g. you see a shiny jewel here.")
  room = models.ForeignKey(Room, help_text="Where can this item be found?")
- owner = models.ForeignKey(Person, blank=True, null=True, help_text="Leave this blank.")
- takeable = models.BooleanField(default=True, help_text="Can this object be taken?") # can this item be moved?
+ visible = models.BooleanField(default=True, help_text="Can this object be seen and taken?")
  def __unicode__(self):
   return self.name
- def used_on(self, other_item): # this is the victim method - what does this object do when something's used on it?
-  pass
- def use(self, what=None): # What am I using this object on?
-  pass
 
+class Quest(models.Model):
+ name = models.CharField(max_length=50, unique=True)
+ description = models.TextField()
+ items = models.ManyToManyField(Item)
+ time_limit = models.IntegerField(default=0, help_text="How long in seconds does player have to complete the quest? 0 = unlimited")
+ cost = models.IntegerField(default=0) # how much does it cost to go on this quest?
+ payout = models.IntegerField(default=0) # How much do you earn from completing the quest?
+ def __unicode__(self):
+  return self.name
+  
--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
 
-urlpatterns = patterns(
-    url(r'^game$', 'venture.views.game'),
+urlpatterns = patterns("", 
+    url(r'^game/$', 'venture.views.game'),
     url(r'^action/$', 'venture.views.action'),
     url(r'^quit/$', 'venture.views.quit'),
     url(r'^login/$', 'venture.views.login'),
-    url(r'^$', 'venture.views.main'),
+    # url(r'^$', 'venture.views.login'),
 )
